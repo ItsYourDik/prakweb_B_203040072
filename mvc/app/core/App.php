95b028257c85668050a4c5372d0a7f8224bd0f45
ItsYourDik <203040072@mail.unpas.ac.id>
2022-10-09 19:28:55 +0700

    Menyelesaikan Routing

--- mvc/app/core/App.php
+++ mvc/app/core/App.php
@@ -4,6 +4,17 @@ class App
 {
   public function __construct()
   {
-    echo "OK!";
+    $url = $this->parsesURL();
+    var_dump($url);
+  }
+
+  public function parsesURL()
+  {
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
   }
 }
