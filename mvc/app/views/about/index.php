9b273ff65387c3dc139ebafa20844184e2a9e63c
ItsYourDik <203040072@mail.unpas.ac.id>
2022-10-11 22:46:13 +0700

    menyelesaikan aseets

--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,4 +1,5 @@
 <div class="container">
   <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/dik.jpg" alt="Dik DIk Nur Illahi" width="200">
   <p>Hallo, Nama Saya <?= $data['nama'] ?>, Umur saya <?= $data['umur'] ?>, Saya adalah Seorang <?= $data['pekerjaan'] ?>.</p>
 </div>
